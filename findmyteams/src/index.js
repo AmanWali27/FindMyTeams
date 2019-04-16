4124c7a87a0c115a212d46ab4fd0f965cff31f76
Aman Wali <amanwali27@gmail.com>
2019-04-15 21:43:55 -0400

    Init App
    Init app and display test text

---
+++ findmyteams/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            Test index
+        </div>
+    );
+};
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);
