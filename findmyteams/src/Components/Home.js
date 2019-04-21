b07bf2f4ae87f44325521e42a134a6c167a8a66e
Anvi Mehta <mehta109@purdue.edu>
2019-04-20 20:37:09 -0400

    home page
    creating top bar for finding a team or player, and inserting a fab button at the bottom right side of the page

--- findmyteams/src/Components/Home.js
+++ findmyteams/src/Components/Home.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Container, Button, Link } from 'react-floating-action-button'
 
 const Home = () =>{
     console.log("HELLO");
     return(
         <div>
-            This is the home screen after logging in!
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100px'}}>
+            <div className="ui compact menu">
+                <a className="item">
+                    Find a Player
+                </a>
+                <div className="link item">
+                    Find a Team
+                </div>
+            </div>
+        </div>
+
+            <Container>
+                <Button
+                    tooltip="The big plus button!"
+                    icon="fas fa-plus"
+                    rotate={true}
+                    onClick={() => alert('FAB Rocks!')} />
+            </Container>
         </div>
     );
 };
