2ee73caab966b1f07c5b50f79956a04b985c4a7a
Anvi Mehta <mehta109@purdue.edu>
2019-04-24 23:56:37 -0400

    Phone number to firebase
    stored the phone number in the firebase to the user.

--- findmyteams/src/Components/Phone.js
+++ findmyteams/src/Components/Phone.js
@@ -2,8 +2,38 @@ import React from 'react';
 import Paper from "@material-ui/core/Paper";
 import styles from "./background";
 import Header from './Header'
+import {auth, db} from "../fbase";
 
 class Phone extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            phoneNumber: ""
+        };
+    }
+
+    onChangeNumber(event) {
+        this.setState({phoneNumber: event.target.value})
+    }
+
+    handleSubmit= (e) => {
+        e.preventDefault();
+        console.log("About to submit");
+        const u=auth.currentUser.uid;
+        const userRef = db.ref("Users/" + u + "/");
+        userRef.update(
+            {
+                phoneNumber: this.state.phoneNumber
+            }
+        ).then((data) => {
+            console.log('form sent');
+            //this.props.navigation.navigate('requests')
+        }).catch((error) => {
+            console.log(error)
+        });
+    };
+
     render () {
         return (
             <div>
@@ -15,10 +45,16 @@ class Phone extends React.Component {
                             <div className="field">
                                 <label style={{fontSize:'25px'}}>Please Enter Your Phone Number</label>
                                 <br/>
-                                <input style={{fontSize:'17px'}} type="text" placeholder="Phone Number"/>
+                                <input
+                                    style={{fontSize:'17px'}}
+                                    type="text"
+                                    placeholder="Phone Number"
+                                    value={this.state.phoneNumber}
+                                    onChange={this.onChangeNumber.bind(this)}
+                                />
                             </div>
                         </div>
-                        <button style={{fontSize:'17px'}} className="ui large teal button">
+                        <button onClick={this.handleSubmit} style={{fontSize:'17px'}} className="ui large teal button">
                             Save
                         </button>
                     </div>
